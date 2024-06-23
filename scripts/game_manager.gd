3d659b30e275c786e24d39eac7fdcd6b369e7f52
lian <ian.sgv13@gmail.com>
2024-06-23 17:40:03 -0400

    cambio del map
    cambio del map y primera parte para el changescenes

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,11 +1,13 @@
 extends Node
 
+var player: CharacterBody2D
+var scenes_path : String = "res://scenes/"
 
 
-
-
-func change_scene() -> void:
-	pass
-
+func change_scene(from, to_scene_name : String) -> void:
+	player = from.player
+	player.get_parent().remove_child(player)
+	var fullpath = scenes_path + to_scene_name + "tscn"
+	from.get_tree().call_deferred("change_scene_to_file", fullpath)
 
 
