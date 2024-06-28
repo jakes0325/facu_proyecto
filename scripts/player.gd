2520869add6fec8fa57df832fcfc3431054c18ad
lian <ian.sgv13@gmail.com>
2024-06-27 23:16:15 -0400

    chat fix
    chatting fix :V

--- scripts/player.gd
+++ scripts/player.gd
@@ -10,31 +10,32 @@ func _physics_process(delta):
 	player_movement(delta)
 
 func player_movement(delta) -> void:
-	if Input.is_action_pressed("ui_right"):
-		current_dir = "right"
-		play_anim(1)
-		velocity.x = SPEED
-		velocity.y = 0
-	elif Input.is_action_pressed("ui_left"):
-		current_dir = "left"
-		play_anim(1)
-		velocity.x = -SPEED
-		velocity.y = 0
-	elif Input.is_action_pressed("ui_down"):
-		current_dir = "down"
-		play_anim(1)
-		velocity.y = SPEED
-		velocity.x = 0
-	elif Input.is_action_pressed("ui_up"):
-		current_dir = "up"
-		play_anim(1)
-		velocity.y = -SPEED
-		velocity.x = 0
-	else:
-		play_anim(0)
-		velocity.y = 0
-		velocity.x = 0
-	move_and_slide()
+	if GameManager.chatting == false:
+		if Input.is_action_pressed("ui_right"):
+			current_dir = "right"
+			play_anim(1)
+			velocity.x = SPEED
+			velocity.y = 0
+		elif Input.is_action_pressed("ui_left"):
+			current_dir = "left"
+			play_anim(1)
+			velocity.x = -SPEED
+			velocity.y = 0
+		elif Input.is_action_pressed("ui_down"):
+			current_dir = "down"
+			play_anim(1)
+			velocity.y = SPEED
+			velocity.x = 0
+		elif Input.is_action_pressed("ui_up"):
+			current_dir = "up"
+			play_anim(1)
+			velocity.y = -SPEED
+			velocity.x = 0
+		else:
+			play_anim(0)
+			velocity.y = 0
+			velocity.x = 0
+		move_and_slide()
 
 func play_anim(movement) -> void:
 	var dir = current_dir
