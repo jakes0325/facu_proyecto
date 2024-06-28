d0c92fcc8377d4030eb85bc6b5e5177cc70b32e8
lian <ian.sgv13@gmail.com>
2024-06-27 21:23:41 -0400

    terminando cambio
    terminando cambio de escenas

--- scripts/Followcam.gd
+++ scripts/Followcam.gd
@@ -4,14 +4,14 @@ class_name camera extends Camera2D
 @export var tilemap: TileMap
 
 func _ready():
-	var mapRect = tilemap.get_used_rect()
-	var tileSize = tilemap.rendering_quadrant_size
-	var worldSizeInPixels = mapRect.size * tileSize
-	limit_right = worldSizeInPixels.x
-	limit_bottom = worldSizeInPixels.y
-	limit_left = 0
-	limit_top = 0
-	
+	#var mapRect = tilemap.get_used_rect()
+	#var tileSize = tilemap.rendering_quadrant_size
+	#var worldSizeInPixels = mapRect.size * tileSize
+	#limit_right = worldSizeInPixels.x
+	#limit_bottom = worldSizeInPixels.y
+	#limit_left = 0
+	#limit_top = 0
+	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	global_position = follow_node.global_position
