e8c0396f0e01891b1a552364dec231991a7da4f9
Pablo Gamarra <p.gamarra.jara.1503@gmail.com>
2024-07-24 14:46:19 -0400

    Dead screen agregada

---
+++ scripts/muerte.gd
@@ -0,0 +1,18 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$BtnPlayAgain.grab_focus()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_button_pressed():
+	print("VOLVER A JUGAR")
+	GameManager.play_again()
+
+func _on_button_2_pressed():
+	print("MENU PRINCIPAL")
+	GameManager.back_to_menu()
