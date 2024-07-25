3e98d2c17787019607d75d39925e482d9bfcc9f7
Pablo Gamarra <p.gamarra.jara.1503@gmail.com>
2024-07-25 11:57:34 -0400

    Cambio de nombre de escena

--- scripts/dead_screen.gd
+++ scripts/dead_screen.gd
@@ -1,18 +1,23 @@
-extends Node
+extends Control
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$PanelContainer/MarginContainer/BoxContainer2/BtnPlayAgain.grab_focus()
+	self.grab_focus()
+	$PanelContainer/MarginContainer/BoxContainer2/BtnPlay
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 func _on_button_pressed():
-	print("VOLVER A JUGAR")
+	get_tree().paused = false
+	
+	print("JUGAR")
 	GameManager.play_again()
 
 func _on_button_2_pressed():
-	print("MENU PRINCIPAL")
+	get_tree().paused = false
+		
+	print("SALIR")
 	GameManager.back_to_menu()
